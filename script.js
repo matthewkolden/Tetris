60fca3ce5523185aab15dd748150851b927cbc83
matthewkolden <matthewkolden2k@gmail.com>
2022-11-28 22:08:04 -0800

    created game grid

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Declare Variables
+const rows = 20;
+const columns = 10;
+
+const scoreEl = document.getElementById("score");
+const startBtnEl = document.getElementById("start-btn");
+const gameGridEl = document.getElementById("game-grid");
+const gridBlocks = document.querySelectorAll("#game-grid .grid-block");
 
 // Functions
+
 
 // Event Listeners
