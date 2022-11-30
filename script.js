96c6ab223cf9581a20092cc89907d20ddedcb675
matthewkolden <matthewkolden2k@gmail.com>
2022-11-30 01:10:57 -0800

    switched gameboard to canvas

--- script.js
+++ script.js
@@ -4,10 +4,40 @@ const columns = 10;
 
 const scoreEl = document.getElementById("score");
 const startBtnEl = document.getElementById("start-btn");
-const gameGridEl = document.getElementById("game-grid");
-const gridBlocks = document.querySelectorAll("#game-grid .grid-block");
+
+const gameBoardEl = document.getElementById("game-grid");
+gameBoardEl.width = 400;
+gameBoardEl.height = 800;
+
+const ctx = gameBoardEl.getContext("2d");
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 40, 40);
+
 
 // Functions
 
-
 // Event Listeners
+// startBtnEl.addEventListener("click", () => {
+//     drawBlock(x, y)
+
+// })
+// ctx.fillRect(x, y, 40, 40);
+
+// document.addEventListener("keydown", (key) => {
+//     if(key.code == "KeyA" && x <= 400) {
+//         x -= 40;
+//     }
+//     if(key.code == "KeyD" && x >= 0) {
+//         x += 40;
+//     }
+//     if(key.code == "KeyS" && y <= 800) {
+//         y -= 40;
+//     }
+    // if(key.code === "ArrowUp") {
+
+    // }
+//     ctx.clearRect(400, 800, 40, 40);
+//     ctx.fillRect(x, y, 40, 40);
+//     console.log(key.code)
+// })
